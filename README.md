85860d2f2fc34b05d14c38c204f32b06652d2863
armanmulani <armanmulani2212@gmail.com>
2025-12-11 22:44:04 +0530

    Todo App backend done

---
+++ README.md
@@ -0,0 +1,9 @@
+## Todo app
+This project contains a simple TODO application
+It has the following features:
+
+ - Anyone can create a Todo
+ - Anyone can see their existing todo
+ - Anyone can mark a Todo as done
+
+//Initialize a node project 
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require ('express');
+const {createTodo} = require('./types')
+
 const app = express();
 const port = 3000;
 
@@ -6,16 +8,47 @@ const port = 3000;
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-
+app.get('/todos', (req, res) => {
+   
 })
 
-app.post('./todo', (req,res) => {
+app.post('./todo', async function (req,res) {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg:"Invalid input data"
+        })
+        return;
+    }
 
+    await todo.create({
+        title:createPayload.create,
+        description:createPayload.create
+    })
+    res.json({
+        msg:"Todo created"
+    })
 })
 
-app.post('./completed', (req,res) => {
+app.get('./todos', async (req, res) => {
+    const todos = await todos.find({
 
+    })
+    res.json({
+        todos
+    })
+})
+
+app.put('./completed', (req,res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg:"Invalid input data"
+        })
+        return;
+    }
 })
 app.delete('./drop', (req, res) => {
 
